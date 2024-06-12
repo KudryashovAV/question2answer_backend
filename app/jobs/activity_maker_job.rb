90e085900eacd7de628494338f3719401e692cd9
Alexey Kudryashov <akd@krononsoft.com>
2024-06-12 11:36:20 +0300

    finish production ActiveJob workers checking

--- app/jobs/activity_maker_job.rb
+++ app/jobs/activity_maker_job.rb
@@ -2,9 +2,7 @@ class ActivityMakerJob < ActiveJob::Base
   queue_as :default
 
   def perform
-    puts "HERE Inside!!!"
     ActivityMaker.call("question")
     ActivityMaker.call("answer")
-    FeatureFlag.create(name: Faker::Lorem.question(word_count: 5), enabled: false)
   end
 end
