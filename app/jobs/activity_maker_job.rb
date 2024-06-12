f0437a16069a7b43a551e4bd4ed1ca5c3e7b8395
Alexey Kudryashov <akd@krononsoft.com>
2024-06-12 11:16:52 +0300

    Check ActiveJob on production

--- app/jobs/activity_maker_job.rb
+++ app/jobs/activity_maker_job.rb
@@ -5,5 +5,6 @@ class ActivityMakerJob < ActiveJob::Base
     puts "HERE Inside!!!"
     ActivityMaker.call("question")
     ActivityMaker.call("answer")
+    FeatureFlag.create(name: Faker::Lorem.question(word_count: 5), enabled: false)
   end
 end
--- app/jobs/temporary_job.rb
+++
@@ -1,7 +0,0 @@
-class TemporaryJob < ActiveJob::Base
-  queue_as :default
-
-  def perform
-    puts "I am a temporary job"
-  end
-end
